524382337b6f05377504daa346c1d79547cd54bf
Rubens Praser Júnior <praser@gmail.com>
2017-12-24 03:12:22 -0200

    Create authentication in server side

---
+++ backend/app/controllers/authentications_controller.rb
@@ -0,0 +1,18 @@
+class AuthenticationsController < ApplicationController
+  def authenticate
+    @user = User.find_by(email: user_params[:email]).try(:authenticate, user_params[:password])
+
+    if @user
+      render json: @user, status: :created, location: @user
+    else
+      render json: {error: 'Invalid credentials'}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # Only allow a trusted parameter "white list" through.
+  def user_params
+    params.permit(:email, :password)
+  end
+end
