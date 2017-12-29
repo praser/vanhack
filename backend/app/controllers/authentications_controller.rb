d5ebe8aceb9b87436bcbd515943d5a761d26992e
Rubens Praser Júnior <praser@gmail.com>
2017-12-28 22:23:28 -0200

    Change response code from 422 to 401 when a user cannot be authenticated

--- backend/app/controllers/authentications_controller.rb
+++ backend/app/controllers/authentications_controller.rb
@@ -1,11 +1,10 @@
 class AuthenticationsController < ApplicationController
   def authenticate
     @user = User.find_by(email: user_params[:email]).try(:authenticate, user_params[:password])
-
     if @user
       render json: @user, status: :created, location: @user
     else
-      render json: {error: 'Invalid credentials'}, status: :unprocessable_entity
+      render json: {error: 'Invalid credentials'}, status: :unauthorized
     end
   end
 
